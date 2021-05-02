4115bcf5312a7a53c493172b1d940022c256c22a
krishna.gupta <krishna.gupta@go-jek.com>
2021-05-02 08:06:41 +0530

    Add CreateUUID method for DB

--- internal/data/data.go
+++ internal/data/data.go
@@ -8,6 +8,7 @@ import (
 	"github.com/pokekrishna/chitchat/internal/config"
 	"github.com/pokekrishna/chitchat/pkg/log"
 	_ "github.com/lib/pq"
+	"crypto/rand"
 	"time"
 )
 
@@ -36,5 +37,23 @@ func Initialize() error {
 
 func Encrypt(plainText string) (encryptedText string) {
 	encryptedText = fmt.Sprintf("%x", sha1.Sum([]byte(plainText)))
+	return
+}
+
+// create a random UUID with from RFC 4122
+// adapted from http://github.com/nu7hatch/gouuid
+func createUUID() (uuid string) {
+	u := new([16]byte)
+	_, err := rand.Read(u[:])
+	if err != nil {
+		log.Error("Cannot generate UUID", err)
+	}
+
+	// 0x40 is reserved variant from RFC 4122
+	u[8] = (u[8] | 0x40) & 0x7F
+	// Set the four most significant bits (bits 12 through 15) of the
+	// time_hi_and_version field to the 4-bit version number.
+	u[6] = (u[6] & 0xF) | (0x4 << 4)
+	uuid = fmt.Sprintf("%x-%x-%x-%x-%x", u[0:4], u[4:6], u[6:8], u[8:10], u[10:])
 	return
 }
