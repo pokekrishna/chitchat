c9ef5ee90387de4f4805f2774c6b3a1f2025bf5d
krishna.gupta <krishna.gupta@gojek.com>
2021-05-10 00:12:11 +0530

    Add a new pkg 'enhancederror'
    This new package is introduced because the need to compare the errors
    is real and the '==' does not work if errors are of different types.
    
    The errors package provides a funtion 'errors.Is' to compare the error
    but seems as though it expects errors to be created by the package itself
    or it wont work because of interface type assertions

---
+++ pkg/enhancederror/enhancederror.go
@@ -0,0 +1,16 @@
+package enhancederror
+
+import "reflect"
+
+
+func IsEqual(source error, target error) bool{
+	if source == nil && target == nil {
+		return true
+	} else if reflect.TypeOf(source) == reflect.TypeOf(target) &&
+		source.Error() == target.Error() {
+		return true
+	} else {
+		return false
+	}
+}
+
---
+++ pkg/enhancederror/enhancederror_test.go
@@ -0,0 +1,83 @@
+package enhancederror_test
+
+import (
+	"github.com/pokekrishna/chitchat/pkg/enhancederror"
+	"testing"
+)
+import "errors"
+
+type testcase struct{
+	source error
+	target error
+	expect bool
+}
+
+type CustomErr struct {
+	msg string
+}
+
+func (c *CustomErr) Error() string {
+	return c.msg
+}
+
+var testcases = []testcase{
+	{
+		source: nil,
+		target: nil,
+		expect: true,
+	},
+
+	{
+		source: errors.New("flag1"),
+		target: nil,
+		expect: false,
+	},
+
+	{
+		source: nil,
+		target: errors.New("flag2"),
+		expect: false,
+	},
+
+	{
+		source: errors.New("flag3"),
+		target: errors.New("flag4"),
+		expect: false,
+	},
+
+	{
+		source: &CustomErr{msg: "flag1"},
+		target: nil,
+		expect: false,
+	},
+
+	{
+		source: nil,
+		target: &CustomErr{msg: "flag1"},
+		expect: false,
+	},
+
+	{
+		source: &CustomErr{msg: "flag1"},
+		target: &CustomErr{msg: "flag2"},
+		expect: false,
+	},
+
+	{
+		source: &CustomErr{msg: "flag3"},
+		target: &CustomErr{msg: "flag3"},
+		expect: true,
+	},
+}
+
+
+
+func TestIsEqual(t *testing.T) {
+	for _, tc := range testcases{
+		got := enhancederror.IsEqual(tc.source, tc.target)
+		if  got != tc.expect{
+			t.Errorf("IsEqual(%v,%v) returned %v, expected %v.",
+				tc.source, tc.target, got, tc.expect)
+		}
+	}
+}
